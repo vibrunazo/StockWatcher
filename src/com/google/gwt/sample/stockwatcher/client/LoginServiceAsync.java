bcf97d1f776a502c672a03c9bc7e11d303278294
Vandré Brunazo <vibrunazo@gmail.com>
2010-12-08 19:10:28 -0200

    first commit

---
+++ src/com/google/gwt/sample/stockwatcher/client/LoginServiceAsync.java
@@ -0,0 +1,10 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface LoginServiceAsync {
+
+	//	void login(String requestUri, AsyncCallback<LoginInfo> callback);
+	public void login(String requestUri, AsyncCallback<LoginInfo> async);
+
+}
